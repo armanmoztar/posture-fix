e41b183fa8420e0cb1c6b2b9184158a6d500546e
Arman Moztarzadeh <armanmoztar@outlook.com>
2022-06-05 14:17:33 -0700

    update changes

---
+++ README.md
@@ -0,0 +1,3 @@
+
+# PostureFix
+A chrome extention that alerts you to fix your posture from time to time.
--- content.js
+++ content.js
@@ -1,15 +1,14 @@
+
 var startingMinutes = 30;
 let time = startingMinutes * 60;
-
-const countdownEl = document.getElementById('countdown');
-
+var audio = new Audio('/audio.mp3');
 setInterval(updateCountdown, 1000)
-
 function updateCountdown() {
+    const countdown = document.getElementById('countdown');
     const minutes = Math.floor(time / 60);
     let seconds = time % 60;
     seconds = seconds < 10 ? '0' + seconds : seconds;
-    countdownEl.innerHTML = `${minutes}:${seconds}`;
+    countdown.innerHTML = `${minutes}:${seconds}`;
     time--;
     time = time < 0 ? 0 : time;
     if (time == 0) {
@@ -17,8 +16,11 @@ function updateCountdown() {
         time = startingMinutes * 60;
         return;
     }
-    
+
     function fn() {
+        audio.play();
         alert("Remember to fix your posture!")
     }
+    chrome.action.onClicked.addListener(handleActionClick);
 }
+
