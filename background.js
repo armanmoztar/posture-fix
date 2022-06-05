a1aedd2f9726105109b613ef3553001dc4cc4a71
Arman Moztarzadeh <armanmoztar@outlook.com>
2022-06-05 01:17:43 -0700

    updated changes to loading ext

--- background.js
+++ background.js
@@ -8,10 +8,7 @@ setInterval(updateCountdown, 1000)
 function updateCountdown() {
     const minutes = Math.floor(time / 60);
     let seconds = time % 60;
-
     seconds = seconds < 10 ? '0' + seconds : seconds;
-
-
     countdownEl.innerHTML = `${minutes}:${seconds}`;
     time--;
     time = time < 0 ? 0 : time;
@@ -21,23 +18,7 @@ function updateCountdown() {
         return;
     }
     
-
     function fn() {
-        console.log("timer reset");
         alert("Remember to fix your posture!")
     }
 }
-
-// // Reset timer
-// const resetAction = document.getElementById("reset-timer");
-
-// // Function to reset Countdown
-// const resetTimer = () => {
-//     stopTimer();
-//     countDownTime = defaultValue;
-//     renderTime();
-// };
-
-// resetAction.onclick = resetTimer;
-
-// setTimeout(function () { alert("Remember to fix your posture!"); }, time)}
