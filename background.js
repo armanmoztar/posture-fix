21ef3701a354e572eca8ca34ad32db34110803b9
Arman Moztarzadeh <armanmoztar@outlook.com>
2023-06-12 12:28:41 -0700

    fixed bug that prevented notification appearing

--- background.js
+++ background.js
@@ -1,12 +1,18 @@
-chrome.alarms.create({ delayInMinutes: 15, periodInMinutes: 15 });
-
 chrome.alarms.onAlarm.addListener(function (alarm) {
   if (alarm.name === "postureFix") {
+    console.log("Alarm Triggered");
     chrome.notifications.create({
       type: "basic",
       iconUrl: "images/icon128.png",
       title: "Posture Fix",
-      message: "Remember to check your posture!",
+      message: "Remember to check your posture!"
     });
   }
+});
+
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.alarms.create("postureFix", {
+    delayInMinutes: 15,
+    periodInMinutes: 15
+  });
 });
